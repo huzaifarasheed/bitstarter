77c4f399d7c4d99e84b286aef001848b80e87119
huzaifarasheed <huzaifarasheed13@gmail.com>
2013-07-22 20:02:46 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs=require('fs');
+var htmlfile="index.html";
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var html=fs.readFileSync(htmlfile).toString();
+    response.send(html);
+
 });
 
 var port = process.env.PORT || 5000;
